633975cfafd4a0e3e58c0e282364cf5d2fc78127
Florentino Navarro Diaz <tino.navarrro@hotmail.com>
2022-10-17 13:05:07 -0500

    waiting for feedback

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,7 @@
 const Banner = ({ img }) => {
   return (
     <div className="Banner">
-      <img src={img} alt="Banner" />
+      <img src={img} alt="Banner" data-aos="zoom-in-dow" />
     </div>
   );
 };
