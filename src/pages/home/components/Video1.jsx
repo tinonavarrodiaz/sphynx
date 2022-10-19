0654524c9eb8833bf399417b679fcedef34daf46
Florentino Navarro Diaz <tino.navarrro@hotmail.com>
2022-10-19 15:59:27 -0500

    done

--- src/pages/home/components/Video1.jsx
+++ src/pages/home/components/Video1.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useRef } from 'react';
+import { isMobile } from '../../../helpers/isMobile';
+
 export default function Video1({ img }) {
+  const videoEl = useRef(null);
+  const imgElemen = '<img src="/img/home1.jpg">';
+  const videoElement = `
+    <video autoPlay playsInline disablePictureInPicture muted loop id="video">
+      <source src="/media/Intro_Sphynx-720.mp4" type="video/mp4"/>
+    </video>
+  `;
+  useEffect(() => {
+    console.log(isMobile);
+    isMobile
+      ? videoEl.current.insertAdjacentHTML('beforebegin', imgElemen)
+      : videoEl.current.insertAdjacentHTML('beforebegin', videoElement);
+    const video = videoEl.current.querySelector('video')
+      ? videoEl.current.querySelector('video').play()
+      : null;
+  }, []);
   return (
-    <div className="Video">
-      <img src={img} alt="Video" />
+    <div className="Video" ref={videoEl}>
+      {/* <img src={'/img/video.jpg'} alt="Video" /> */}
     </div>
   );
 }
