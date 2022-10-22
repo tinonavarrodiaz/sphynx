3b4737c2f481e0a3b9d4b20da0fb6e446b57f564
Florentino Navarro Diaz <tino.navarrro@hotmail.com>
2022-10-22 15:14:42 -0500

    webp add

--- src/components/Picture.jsx
+++ src/components/Picture.jsx
@@ -5,33 +5,45 @@ const Picture = ({ img, ext, alt }) => {
         srcset={`/img/${img}.webp`}
         type="image/webp"
         media="(min-width: 1024px)"
+        width={'1024px'}
+        height={'auto'}
       />
       <source
         srcset={`/img/${img}.${ext}`}
         type={`image/${ext}`}
         media="(min-width: 1024px)"
+        width={'1024px'}
+        height={'auto'}
       />
       <source
         srcset={`/img/${img}-m.webp`}
         type="image/webp"
         media="(max-width:1024px)"
+        width={'768px'}
+        height={'auto'}
       />
       <source
         srcset={`/img/${img}-m.${ext}`}
         type={`image/${ext}`}
         media="(maz-width:1024px)"
+        width={'768px'}
+        height={'auto'}
       />
       <source
         srcset={`/img/${img}-s.webp`}
         type="image/webp"
         media="(max-width:768px)"
+        width={'768px'}
+        height={'auto'}
       />
       <source
         srcset={`/img/${img}-s.${ext}`}
         type={`image/${ext}`}
         media="(maz-width:768px)"
+        width={'768px'}
+        height={'auto'}
       />
-      <img src={`/img/${img}.${ext}`} alt={alt} />
+      <img src={`/img/${img}.${ext}`} alt={alt} width="auto" height="auto" />
     </picture>
   );
 };
