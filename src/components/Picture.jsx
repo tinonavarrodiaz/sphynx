d3a125e2f7801bd581a219e7311895a05632f435
Florentino Navarro Diaz <tino.navarrro@hotmail.com>
2022-10-25 09:51:45 -0500

    modal fixed

--- src/components/Picture.jsx
+++ src/components/Picture.jsx
@@ -2,42 +2,42 @@ const Picture = ({ img, ext, alt }) => {
   return (
     <picture>
       <source
-        srcset={`/img/${img}.webp`}
+        srcSet={`/img/${img}.webp`}
         type="image/webp"
         media="(min-width: 1024px)"
         width={'1024px'}
         height={'auto'}
       />
       <source
-        srcset={`/img/${img}.${ext}`}
+        srcSet={`/img/${img}.${ext}`}
         type={`image/${ext}`}
         media="(min-width: 1024px)"
         width={'1024px'}
         height={'auto'}
       />
       <source
-        srcset={`/img/${img}-m.webp`}
+        srcSet={`/img/${img}-m.webp`}
         type="image/webp"
         media="(max-width:1024px)"
         width={'768px'}
         height={'auto'}
       />
       <source
-        srcset={`/img/${img}-m.${ext}`}
+        srcSet={`/img/${img}-m.${ext}`}
         type={`image/${ext}`}
         media="(maz-width:1024px)"
         width={'768px'}
         height={'auto'}
       />
       <source
-        srcset={`/img/${img}-s.webp`}
+        srcSet={`/img/${img}-s.webp`}
         type="image/webp"
         media="(max-width:768px)"
         width={'768px'}
         height={'auto'}
       />
       <source
-        srcset={`/img/${img}-s.${ext}`}
+        srcSet={`/img/${img}-s.${ext}`}
         type={`image/${ext}`}
         media="(maz-width:768px)"
         width={'768px'}
