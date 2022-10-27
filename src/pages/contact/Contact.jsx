7594562ae3e9c5c47f68285c0cef58b67624a193
Florentino Navarro Diaz <tino.navarrro@hotmail.com>
2022-10-27 14:59:12 -0500

    contat

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,4 +1,9 @@
 import { useState, useEffect, useRef } from 'react';
+import sgMail from '@sendgrid/mail';
+
+sgMail.setApiKey(
+  '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'
+);
 
 const Contact = () => {
   const [send, setSend] = useState(false);
@@ -8,6 +13,24 @@ const Contact = () => {
 
   const submit = (e) => {
     e.preventDefault();
+    const data = new FormData(e.target);
+    const msg = {
+      to: 'tino.navarro@hotmail.com', // Change to your recipient
+      from: 'tino.navarrod@gmail.com', // Change to your verified sender
+      subject: 'Sending with SendGrid is Fun',
+      text: 'and easy to do anywhere, even with Node.js',
+      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    };
+
+    sgMail
+      .send(msg)
+      .then((response) => {
+        console.log(response[0].statusCode);
+        console.log(response[0].headers);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
     // const uri = e.target.action
     // const method = 'POST'
   };
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import mkcert from 'vite-plugin-mkcert';
 
 import { qrcode } from 'vite-plugin-qrcode';
 import removeConsole from 'vite-plugin-remove-console';
@@ -7,6 +8,7 @@ import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: { https: true },
   plugins: [
     react(),
     removeConsole(),
@@ -19,6 +21,7 @@ export default defineConfig({
         },
       ],
     }),
+    mkcert(),
   ],
   // base: '/sphynx/',
 });
