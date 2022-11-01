42f4e47a966c41746997d431e7fd594bc798d25d
Florentino Navarro Diaz <tino.navarrro@hotmail.com>
2022-10-31 18:47:33 -0600

    map add

--- src/pages/buy/components/Map.jsx
+++ src/pages/buy/components/Map.jsx
@@ -6,14 +6,14 @@ function Map() {
       var a = document.createElement('script');
       a.type = 'text/javascript';
       a.async = !0;
-      a.src = 'https://cdn.storepoint.co/api/v1/js/162223c3f8333d.js';
+      a.src = 'https://cdn.storepoint.co/api/v1/js/163605f8413e28.js';
       var b = document.getElementsByTagName('script')[0];
       b.parentNode.insertBefore(a, b);
     })();
   }, []);
   return (
     <div className="Map" id={'where-to-buy'} style={{ minHeight: '40vh' }}>
-      <div id="storepoint-container" data-map-id="162223c3f8333d"></div>
+      <div id="storepoint-container" data-map-id="163605f8413e28"></div>
     </div>
   );
 }
